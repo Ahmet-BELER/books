4a7d23e381f6dd7803dcbd2490490a9cd66894c2
C8228-AHMET <81635115+Ahmet-BELER@users.noreply.github.com>
2022-02-28 18:13:03 +0300

    BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,36 +1,57 @@
-import React from "react";
-import Book from "./Book";
-import "./BookList.css";
-import { BookContext } from "../context/BookContext";
+import React from 'react';
+import './BookList.css';
+import Book from './Book';
+import  { BookContext } from '../contexts/BookContext';
+import  { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends React.Component {
-  render() {
-    return (
-        <BookContext.Consumer>
-        {value => {
-            return (
-                <section className="page-section bg-light" id="portfolio">
-                <div className="container">
-                    <div className="text-center">
-                        <h2 className="section-heading text-uppercase">BookFolio</h2>
-                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                    </div>
-                    <div className="row">
 
-                        {value.books.map((book, i) => {
-                            return <Book book={book}
-                                key={i}
-                            />
-                        })}
 
-                    </div>
-                </div>
-            </section>
-            )
-        }}
-    </BookContext.Consumer>
-    );
-  }
+    render() {
+        return (
+
+            <ThemeContext.Consumer>{(contextTheme) => (
+                <BookContext.Consumer>
+                    {contextBook => {
+                    
+                    const { books } = contextBook;
+                    const {changeTheme, isDarkTheme, dark, light } = contextTheme;
+                    const theme = isDarkTheme ? dark : light;
+                
+                    return (
+                        <section className="page-section" style={{background: theme.bg, color:theme.txt}} id="portfolio">
+                        <div className="container">
+                            <div className="text-center">
+                                <h2 className="section-heading text-uppercase">BookFolio</h2>
+                                <h3 className="section-subheading text-muted">Benim Kütüphanem</h3>
+                                <button type="button" className="btn btn-sm btn-info" style={{marginTop: '-70px'}}
+                                onClick={changeTheme}>Change Theme</button>
+                            </div>
+                            <div className="row">
+        
+                                {books.map((book, i) => {
+                                    return <Book book={book}
+                                        key={i}
+                                    />
+                                })}
+        
+                            </div>
+                        </div>
+                    </section>
+                    )
+                }}
+            </BookContext.Consumer>
+            )}
+
+
+            </ThemeContext.Consumer>
+
+
+
+
+        )
+    }
+
 }
 
 export default BookList;
